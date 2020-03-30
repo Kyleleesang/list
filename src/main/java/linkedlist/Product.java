53c8e84d98838f6f6ef7b688a233fa2eea9cb855
Kleesang1 <kleesans@oswego.edu>
2020-03-30 15:21:02 -0400

    testing atom safe mode

--- src/main/java/linkedlist/Product.java
+++ src/main/java/linkedlist/Product.java
@@ -1,8 +1,34 @@
 package linkedlist;
 
-public abstract class Product {
 
-    // From previous assignment
+  public abstract class Product {
+    String ProductName;
+    String Isbn;
+    double UnitPrice;
+
+  // A constructor method that initializes the product name, ISBN number, and unit price
+  public Product(String name, String IsbnNum, double price){
+    ProductName = name;
+    Isbn = IsbnNum;
+    UnitPrice = price;
+  }
+
+  // getProductName() - Returns a String that is the name of the product
+  public String getProductName(){
+    return ProductName;
+  }
+
+  // getIsbn() - Returns a String representing the ISBN (barcode) number
+  public String getIsbn(){
+    return Isbn;
+  }
+
+  // getUnitPrice() - Returns a possible decimal value indicating the unit price for each product
+  public double getUnitPrice(){
+    return UnitPrice;
+  }
+  © 2020 GitHub, Inc.
+}
 
 
 }
--- src/main/java/linkedlist/TaxableProduct.java
+++ src/main/java/linkedlist/TaxableProduct.java
@@ -2,7 +2,13 @@ package linkedlist;
 
 public class TaxableProduct extends Product {
 
-    // From previous assignment
+  public TaxableProduct(String name, String isbn, double unitPrice) {
+      super(name, isbn, unitPrice);
+  }
 
-
+  // Implement values() method
+  @Override
+  public String values() {
+      return getProductName() + ":" + getIsbn() + ":" + getUnitPrice() + ":taxable"; // Fix return string -- JPE
+  }
 }
